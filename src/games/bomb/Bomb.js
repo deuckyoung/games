270126904a7036d38d70c2d3451ee3dfa9f43e4f
cps3upp.develop <cps3upp.develop@gmail.com>
2023-06-01 21:04:20 +0900

    lint 수정

--- src/games/bomb/Bomb.js
+++ src/games/bomb/Bomb.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Button, TextField, styled } from '@mui/material';
 import { Observer, useLocalObservable } from 'mobx-react';
 import { Duration } from 'luxon';
